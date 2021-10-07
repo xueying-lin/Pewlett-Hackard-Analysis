a2075aa6a1bc4c183b5cfa49b528c3fdf29caa63
Sherry <sherrylin2110@gmail.com>
2021-10-07 00:14:36 -0400

    complete the analysis-1

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -5,3 +5,22 @@ SELECT * FROM dept_emp;
 SELECT * FROM dept_manager;
 SELECT * FROM salaries;
 SELECT * FROM titles;
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Create new tables from the query
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
--- schema.sql
+++ schema.sql
@@ -40,8 +40,7 @@ CREATE TABLE titles (
      title VARCHAR NOT NULL,
 	 from_date DATE NOT NULL,
 	 to_date DATE NOT NULL,
-     FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	 PRIMARY KEY (emp_no)
+     FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE dept_emp (
@@ -50,6 +49,8 @@ CREATE TABLE dept_emp (
 	 from_date DATE NOT NULL,
 	 to_date DATE NOT NULL,
      FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	 FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	 PRIMARY KEY (emp_no)
+	 FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
+
+--To delete the unusable table
+DROP TABLE dept_emp CASCADE --CASADE delete the relationship with other tables
