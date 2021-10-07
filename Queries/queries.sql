b672bda4d93c4326778ed083ba06d3aa9f43dadc
Sherry <sherrylin2110@gmail.com>
2021-10-06 21:29:08 -0400

    Create tables

---
+++ Queries/queries.sql
@@ -0,0 +1,7 @@
+-- Query for Confirmation of tavle
+SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
+SELECT * FROM titles;
