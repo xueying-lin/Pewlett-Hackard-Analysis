9a3342956f3a2cf1285a097c0e16bb8f983fa433
Sherry <sherrylin2110@gmail.com>
2021-10-16 17:42:54 -0400

    Deliberable 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -32,3 +32,21 @@ INTO retiring_titles
 From unique_titles
 GROUP BY title
 ORDER BY count(title) DESC;
+
+--Identify current employees who are eligible to mentorship program
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
